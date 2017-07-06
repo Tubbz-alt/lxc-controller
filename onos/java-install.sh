3848ac8e6f8ba958cfa8d8b930a63aa52013c7b4
Jaehyun Nam <namjh@kaist.ac.kr>
2017-07-07 01:00:57 +0900

    add java installer

---
+++ onos/java-install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Oracle java 8
+
+sudo apt-get update
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository -y ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install -y oracle-java8-installer oracle-java8-set-default
