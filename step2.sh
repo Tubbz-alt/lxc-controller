e2a2e8029e7791660d83d159e8d0cb5705dbde13
Jaehyun Nam <namjh@kaist.ac.kr>
2018-01-19 22:33:31 -0800

    Update step2.sh

--- step2.sh
+++ step2.sh
@@ -15,13 +15,13 @@ cp bin/* ~
 # add onos configurations to the configuration of each containter
 
 echo
-echo "Attach ~/lxc-controller/onos/lxc/onos1 at the end of /var/lib/lxc/onos1/config"
-echo "Attach ~/lxc-controller/onos/lxc/onos2 at the end of /var/lib/lxc/onos2/config"
-echo "Attach ~/lxc-controller/onos/lxc/onos3 at the end of /var/lib/lxc/onos3/config"
+echo "Add the contents of ~/lxc-controller/onos/lxc/onos1 at the end of /var/lib/lxc/onos1/config"
+echo "Add the contents of ~/lxc-controller/onos/lxc/onos2 at the end of /var/lib/lxc/onos2/config"
+echo "Add the contents of ~/lxc-controller/onos/lxc/onos3 at the end of /var/lib/lxc/onos3/config"
 echo
 echo "Start onos1, onos2, and onos3 containers"
 echo
-echo "Attach 'ubuntu ALL=NOPASSWD:ALL' at the end of /etc/sudoers"
+echo "Add 'ubuntu ALL=NOPASSWD:ALL' at the end of /etc/sudoers"
 echo "Install Oracle Java 8 (see ~/lxc-controller/onos/java-install.sh)"
 echo "Set eth0 with a static IP address in each container"
 echo
